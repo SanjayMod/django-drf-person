f754a652c3438500f8bebde1159687511c67a032
Sanjay M <smodasia83@gmail.com>
2024-01-29 12:43:11 +0530

    Added Person model in the app

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from app.models import Person
 
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'email'
+    )
+
+
+admin.site.register(Person, PersonAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    """ Model: Person """
+
+    # Field declarations
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
