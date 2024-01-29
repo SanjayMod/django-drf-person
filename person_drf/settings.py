5c937ddf6a3361db762fc7cf19724d0151e4674f
Sanjay M <smodasia83@gmail.com>
2024-01-29 11:56:36 +0530

    Configured djangorestframework package in the project

--- person_drf/settings.py
+++ person_drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-((w7h7)wdrp!(5!i26o&842-dl*@dan4%_hp)a7c-u_m3b=d32
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external python packages
+
+    # For Django REST framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
